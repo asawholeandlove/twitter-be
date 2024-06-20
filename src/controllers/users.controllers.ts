55d34ffa2a08f4417fc56366d223d2cb296520e5
dangthaii <thaidv@hivetech.vn>
2024-06-20 09:02:05 +0700

    tạo access token và refresh token

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -16,8 +16,8 @@ export const loginController = (req: Request, res: Response) => {
 }
 
 export const registerController = async (req: Request, res: Response) => {
-  const { email, password } = req.body
-  const result = await usersService.register({ email, password })
+  req.body
+  const result = await usersService.register(req.body)
 
   return res.status(201).json({
     message: 'User created successfully',
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,10 +1,44 @@
 import User from '~/models/schemas/User.schema'
 import db from './database.services'
+import { hashPassword } from '~/utils/crypto'
+import { RegisterReqBody } from '~/models/schemas/requests/User.requests'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enums'
 
 class UsersService {
-  async register({ password, email }: { email: string; password: string }) {
-    const result = await db.users.insertOne(new User({ email, password }))
-    return result
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.AccessToken
+      },
+      options: {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.RefreshToken
+      },
+      options: {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+
+  async register(payload: RegisterReqBody) {
+    const { password, date_of_birth } = payload
+
+    const result = await db.users.insertOne(
+      new User({ ...payload, password: hashPassword(password), date_of_birth: new Date(date_of_birth) })
+    )
+    const userId = result.insertedId.toString()
+    const [accessToken, refreshToken] = await Promise.all([this.signAccessToken(userId), this.signRefreshToken(userId)])
+
+    return { ...result, accessToken, refreshToken }
   }
   async checkEmailExists(email: string) {
     const result = await db.users.findOne({ email })
